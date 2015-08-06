d5cd4e71e55cbd2aeed13dd759862f0a48edcde7
Lars Janssen <lars@fazy.net>
2015-08-06 15:02:14 +0100

    Tail logs together for consistent output

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 mkdir -p app/cache app/logs
+touch app/logs/prod.log
 chown -R www-data: .
+
 source /etc/apache2/envvars
-tail -F /var/log/apache2/* &
+
+tail -F /var/log/apache2/* app/logs/prod.log &
 exec apache2 -D FOREGROUND
 
