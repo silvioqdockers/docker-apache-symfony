288ae31e8858e17ec6e0e60ea9831357860f01b8
Silvio <silvioq@gmail.com>
2016-12-10 10:12:47 -0300

    Nueva versión para php7

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir -p app/cache app/logs
-touch app/logs/prod.log
+mkdir -p var/cache var/logs var/sessions app/cache app/logs
+touch var/logs/prod.log app/logs/prod.log
 chgrp -R www-data .
-chmod -R g+w app/cache app/logs
+chmod -R g+w var/cache var/logs var/sessions app/cache app/logs
 
 source /etc/apache2/envvars
 
-tail -F /var/log/apache2/* app/logs/prod.log &
+tail -F /var/log/apache2/* app/logs/prod.log var/logs/prod.log &
 exec apache2 -D FOREGROUND
 
